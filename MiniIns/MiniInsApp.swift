791246a1b63e01b507f5e69bd35dec193dda8626
Evelyngyl <934336856@qq.com>
2023-10-03 23:10:54 -0700

    Initial Commit

---
+++ MiniIns/MiniInsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniInsApp.swift
+//  MiniIns
+//
+//  Created by 葛聿泠 on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct MiniInsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
